be4d0c37fbcb400711eb88eabc102044c61cb200
RedWheeler <ethannetsch@gmail.com>
2020-11-19 22:32:13 -0500

    Add Animation Component
    A new built in component, Animation, has been added. This component is
    similar to Sprite - but it cycles through sequential sprites to provide
    built in animation capability. Member variables frameLength and repeat
    were added to control how long each sprite should be displayed and if
    the animation should repeat respectively.

---
+++ Animation.java
@@ -0,0 +1,60 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.util.List;
+import java.util.ArrayList;
+
+class Animation implements Component {
+
+    public GameObject parent;
+    public Scene scene;
+
+    public ArrayList<BufferedImage> frames;
+    public int frameCount;
+    public int frameIndex;
+    public int frameLength; // Amount of frames each sprite should be played for
+    public boolean repeat;
+    public boolean finished;
+    private int lastChange; // How many frames have passed since last sprite change
+
+    public Animation (Scene scene, GameObject parent, List<String> imagePaths, int frameLength, boolean repeat) {
+        this.scene = scene;
+        this.parent = parent;
+        this.frameLength = frameLength;
+        this.repeat = repeat;
+        this.frames = new ArrayList<BufferedImage> ();
+        // attempt to read all image paths 
+        for (String imagePath: imagePaths) {
+            try {
+                frames.add(ImageIO.read(new File(imagePath)));
+            } 
+            catch (Exception e) {
+                System.out.println("Unable to read in image file: " + imagePath + ".");
+            }
+        }
+        this.frameIndex = 0;
+        this.frameCount = this.frames.size();
+    }
+
+    public void update() {
+        if (!this.finished) {
+            this.lastChange++;
+            if (this.lastChange == this.frameLength) {
+                this.lastChange = 0;
+                // If the last frame is reached
+                if (this.frameIndex == this.frames.size()-1) {
+                    if (this.repeat) {
+                        this.frameIndex = 0;
+                    }
+                    else {
+                        this.finished = true;
+                    }
+                }
+                else {
+                    this.frameIndex++;
+                }
+            }
+            this.scene.drawSprite(this.frames.get(this.frameIndex), this.parent.position, this.parent.scale, this.parent.rotation);
+        }
+    }
+}
