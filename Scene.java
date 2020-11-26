6e27169bd41e1f3aeb2861f61989befaac558f31
RedWheeler <ethannetsch@gmail.com>
2020-11-26 15:45:05 -0500

    Add GameObject groups to Scene.java
    Groups have been added to Scene.java. Groups can be used to associate
    game objects within a scene together. For example, a group "enemy" can
    be created that contains all GameObjects that a player's weapon will
    damage. This prevents the player's weapon GameObject from checking
    collisions with all GameObjects in the scene and instead only checks the
    objects it could actually damage. Currently groups can only be added by
    calling addGameObject with a list of strings. Objects cannot be added to
    the same group twice.

--- Scene.java
+++ Scene.java
@@ -1,11 +1,13 @@
 package game_engine;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 
 public class Scene {
     
-    public ArrayList<GameObject> gameObjects; 
+    private HashMap<String, ArrayList<GameObject>> groups; 
+    public ArrayList<GameObject> gameObjects;
     public HashMap<String, Scene> transitions;
 
     public Scene() {
@@ -13,19 +15,57 @@ public class Scene {
         this.transitions = new HashMap<String, Scene> ();
     }
 
+    // Add a gameObject to the scene without a group
     public void addGameObject (GameObject gameObject) {
-        gameObjects.add(gameObject);
+        // Check if gameObjects already contains reference to gameObject
+        if (!this.gameObjects.contains(gameObject)) {
+            this.gameObjects.add(gameObject);
+        }
     }
 
+    // Add a gameObject to the scene with groups
+    public void addGameObject (GameObject gameObject, List<String> groupNames) {
+        this.addGameObject(gameObject);
+        for (String groupName: groupNames) {
+            ArrayList<GameObject> group;
+            // Get list of objects in group if groupName is found
+            if (this.groups.containsKey(groupName)) {
+                group = this.groups.get(groupName);
+            }
+            // Add new list to groupss if groupName is not found
+            else {
+                group = new ArrayList<GameObject> ();
+                this.groups.put(groupName, group);
+            }
+            // Check if group already contains reference to gameObject
+            if (!group.contains(gameObject)) {
+                group.add(gameObject);
+            }
+        }
+    }
+
+    // Return ArrayList of GameObjects within group specified by groupName
+    public ArrayList<GameObject> getGroup (String groupName) {
+        if (groups.containsKey(groupName)) {
+            return groups.get(groupName);
+        }
+        else {
+            return null;
+        }
+    }
+
+    // Add new scene to transition map with specified transitionName
     public void addTransition (String transitionName, Scene nextScene) {
         this.transitions.put(transitionName, nextScene);
     }
 
+    // Return scene in transition table with key transitionName
     public Scene transition (String transitionName) {
         if (transitions.containsKey(transitionName)) {
             return transitions.get(transitionName);
         }
         else {
+            // Should throw exception here
             return this;
         }
     }
