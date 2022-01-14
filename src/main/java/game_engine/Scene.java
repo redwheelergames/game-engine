94c1a727d5e32ce95e38e28832dd644e46cbbffd
RedWheeler <ethannetsch@gmail.com>
2022-01-14 11:15:52 -0500

    Remove deprecated addGameObject methods from Scene

--- src/main/java/game_engine/Scene.java
+++ src/main/java/game_engine/Scene.java
@@ -1,18 +1,6 @@
 package game_engine;
 
 public class Scene {
-    //Added for backwards compatibility
-    public void addGameObject(GameObject gameObject){
-        var sceneManager = gameObject.game.sceneManager;
-        sceneManager.addGameObject(gameObject);
-    }
-
-    //Added for backwards compatibility
-    public void addGameObject(GameObject gameObject, String group){
-        var sceneManager = gameObject.game.sceneManager;
-        sceneManager.addGameObject(gameObject, group);
-
-    }
 
     public void load(Game game){}
 
